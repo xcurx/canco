30ebacdec014ad89afadabf1004784ef5d729418
xcurx <suj5al78@gmail.com>
2026-05-18 14:54:45 +0530

    refactor: remove unused import and variable declaration in server start function

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,12 +1,6 @@
 package server
 
-import (
-	"github.com/xcurx/canco-backend/internal/types"
-)
-
 func Start() {
-	var ot = types.CreateShape
-	println(ot)
 	r := InitializeServer()
 
 	err := r.Run() // listen and serve on
