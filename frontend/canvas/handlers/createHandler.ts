7f45a3780201556b913103f6b308662b5cc6f6db
xcurx <suj5al78@gmail.com>
2026-07-08 22:16:46 +0530

    refactor: move createShape to createHandler and rename to createShapeFromTool

--- frontend/canvas/handlers/createHandler.ts
+++ frontend/canvas/handlers/createHandler.ts
@@ -1,12 +1,13 @@
 import { InteractionContext, InteractionHandler } from './interactionHandler'
 import { CanvasCoords, CanvasState as CanvasStateEnum, ShapeData } from '../type'
-import { CanvasState } from '../state'
+import { CanvasState, createCircleData, createLineData, createRectangleData, createTextData } from '../state'
+import { isShapeViable, updateTempShape } from '../utils'
 
 export class CreateHandler implements InteractionHandler {
     private tempShape: ShapeData | null = null
 
     constructor(private context: InteractionContext, private startPoint: CanvasCoords) {
-        this.tempShape = this.context.toolManager.createShape(startPoint)
+        this.tempShape = this.createShapeFromTool(startPoint)
         if (this.tempShape) {
             this.context.changeState(CanvasStateEnum.CREATING_SHAPE)
             this.context.updateTempShape(this.tempShape)
@@ -15,13 +16,13 @@ export class CreateHandler implements InteractionHandler {
 
     onMouseMove(coords: CanvasCoords, e: PointerEvent): void {
         if (this.tempShape) {
-            this.tempShape = this.context.toolManager.updateTempShape(this.tempShape, this.startPoint, coords)
+            this.tempShape = updateTempShape(this.tempShape, this.startPoint, coords)
             this.context.updateTempShape(this.tempShape)
         }
     }
 
     onMouseUp(coords: CanvasCoords, e: PointerEvent): void {
-        if (this.tempShape && this.context.toolManager.isShapeViable(this.tempShape)) {
+        if (this.tempShape && isShapeViable(this.tempShape)) {
             this.context.applyOperation(CanvasState.createShape(this.tempShape), true)
             if (this.tempShape.type === 'text') {
                 this.context.onEditText?.(this.tempShape)
@@ -34,5 +35,17 @@ export class CreateHandler implements InteractionHandler {
     cleanup(): void {
         this.context.updateTempShape(null) 
         this.context.toolManager.clearTool() 
+    }
+
+    private createShapeFromTool(coords: CanvasCoords): ShapeData | null {
+        const tool = this.context.toolManager.getCurrentTool()
+        const color = this.context.toolManager.getColor()
+        switch (tool) {
+            case 'rectangle': return createRectangleData(coords.x, coords.y, 0, 0, color)
+            case 'circle':    return createCircleData(coords.x, coords.y, 0, 0, color)
+            case 'line':      return createLineData(coords.x, coords.y, coords.x, coords.y, color)
+            case 'text':      return createTextData(coords.x, coords.y, color, "")
+            default:          return null
+        }
     }
 }
