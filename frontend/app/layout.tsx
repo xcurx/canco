7b4cce80d965c2b1803ca863d544be1162445c54
xcurx <suj5al78@gmail.com>
2026-07-12 21:19:29 +0530

    fix: suppressHydrationWarning

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
