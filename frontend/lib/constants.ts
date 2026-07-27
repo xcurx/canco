639d042e66cf717f141b75a27ec49592f9cb0e59
xcurx <suj5al78@gmail.com>
2026-07-27 20:44:55 +0530

    feat: add constants for stroke width and options

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,3 +1,32 @@
+import { RectangleHorizontal, Circle as CircleIcon, LineChartIcon, Type, MousePointer2, Hand } from 'lucide-react'
+
+export const OPTIONS = [
+  {
+    value: "select",
+    icon: MousePointer2,
+  },
+  {
+    value: "pan",
+    icon: Hand,
+  },
+  {
+    value: "rectangle",
+    icon: RectangleHorizontal,
+  },
+  {
+    value: "circle",
+    icon: CircleIcon,
+  },
+  {
+    value: "line",
+    icon: LineChartIcon,
+  },
+  {
+    value: "text",
+    icon: Type,
+  }
+]
+
 export const COLORS = [
   "#ffffff", // white
   "#ced4da", // light gray
@@ -11,3 +40,5 @@ export const COLORS = [
   "#1971c2", // blue
   "#6741d9", // violet
 ]
+
+export const STROKE_WIDTHS = [1, 3, 5]
