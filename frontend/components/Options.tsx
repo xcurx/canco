7b503c2d915e810a5384f1f24c38f23f8d2f20a1
xcurx <suj5al78@gmail.com>
2026-07-27 20:48:08 +0530

    refactor: refactor Options component to use dynamic tool mapping

--- frontend/components/Options.tsx
+++ frontend/components/Options.tsx
@@ -4,6 +4,7 @@ import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
 import { RectangleHorizontal, Circle as CircleIcon, LineChartIcon, Undo, Redo, Type, MousePointer2, Hand } from 'lucide-react'
 import { Button } from './ui/button'
 import { useHistory, useSelectedTool } from '@/lib/hook'
+import { OPTIONS } from '@/lib/constants'
 
 const Options = () => {
     const {renderer} = useContext(RendererContext)
@@ -13,8 +14,6 @@ const Options = () => {
 
     const handleOptionChange = (value: string) => {
         if (!renderer) return
-        console.log("Option changed to: ", value);
-
         renderer.toolManager.setCurrentTool(value as any)
         setCurrentOption(value)
     }
@@ -29,16 +28,18 @@ const Options = () => {
     <div className='dark absolute top-4 left-1/2 -translate-x-1/2 z-10 flex items-center gap-4 glass px-2 py-2 rounded-2xl shadow-xl text-zinc-200'>
         <ToggleGroup 
             type="single" 
-            variant={"outline"} 
+            variant={"default"} 
             value={currentOption}
             onValueChange={(value) => handleOptionChange(value)}
+            className='rounded-xl'
         >
-            <ToggleGroupItem value="select"><MousePointer2/></ToggleGroupItem>
-            <ToggleGroupItem value="pan"><Hand/></ToggleGroupItem>
-            <ToggleGroupItem value="rectangle"><RectangleHorizontal/></ToggleGroupItem>
-            <ToggleGroupItem value="circle"><CircleIcon/></ToggleGroupItem>   
-            <ToggleGroupItem value="line"><LineChartIcon/></ToggleGroupItem>
-            <ToggleGroupItem value="text"><Type/></ToggleGroupItem>
+            {
+                OPTIONS.map(({value, icon: Icon}) => {
+                    return (
+                        <ToggleGroupItem key={value} className='rounded-xl first:rounded-xl last:rounded-xl data-[state=on]:bg-indigo-500/20 data-[state=on]:text-indigo-300 text-zinc-400 hover:bg-white/[0.08] hover:text-white transition-all' value={value}><Icon/></ToggleGroupItem>
+                    )
+                })
+            }
         </ToggleGroup>
         
         {/* Undo/Redo buttons */}
